e963a09ffe0736e6aa52989e7c45e76d13eb2167
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2025-07-22 14:49:40 +0530

    Vercel deployment fixes 3

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,24 @@
-import globals from "globals";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import js from "@eslint/js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default [
-  {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.es2021,
-      },
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
-        },
-        project: path.resolve(__dirname, "./tsconfig.json"),
-      },
-    },
-    plugins: {
-      "react-hooks": pluginReactHooks,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...pluginReactConfig.rules,
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "react/prop-types": "off",
-      "react/react-in-jsx-scope": "off",
+export const eslintConfig = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  rules: {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
   },
-];
+};
+
+export default eslintConfig;
