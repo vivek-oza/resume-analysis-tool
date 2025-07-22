b751479a18fb6b81f903bfa36c0bee29451566e2
vivek-oza <112532672+vivek-oza@users.noreply.github.com>
2025-07-22 15:42:22 +0530

    Minor style fixes

--- src/App.tsx
+++ src/App.tsx
@@ -131,7 +131,7 @@ const App: React.FC = () => {
       <footer className="bg-[#C9EBFF] backdrop-blur-sm text-[#000A16] py-14 px-4 font-sans">
         <div className="flex flex-col gap-14 font-schibsted-grotesk max-w-[1100px] mx-auto">
 
-          <div className="font-medium md:text-4xl text-xl flex flex-col space-y-2">
+          <div className="font-medium md:text-4xl text-2xl flex flex-col space-y-2">
             <div className="">Resume analysis tool</div>
             <div className="">
               
